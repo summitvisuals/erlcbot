2f7249ffc6977a95690ee31e94d74617890b4950
Ben28284 <bgaige552@gmail.com>
2026-05-05 22:12:07 -0600

    Update application command client ID

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
   await rest.put(
-    Routes.applicationCommands('YOUR_CLIENT_ID'),
+    Routes.applicationCommands('1497762509380255865'),
     { body: commands }
   );
 })();
